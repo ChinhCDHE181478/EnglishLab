3977318abfaee7e87e9e961b23431f0187ba5618
ChinhCDHE181478 <quansuper000@gmail.com>
2026-05-26 10:35:21 +0700

    chore: initialize project

---
+++ backend/src/test/java/fu/sap490/g23/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fu.sap490.g23.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
