ff2732d5b12a4beb78ffa128ff4af3134a7797ed
ChinhCDHE181478 <quansuper000@gmail.com>
2026-08-19 13:33:05 +0700

    fix: request mapping fail

---
+++ backend/src/main/java/fu/sep490/g23/backend/controllerv2/placementtest/PlacementTestController.java
@@ -0,0 +1,9 @@
+package fu.sep490.g23.backend.controllerv2.placementtest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/student/placement-tests")
+public class PlacementTestController {
+}
