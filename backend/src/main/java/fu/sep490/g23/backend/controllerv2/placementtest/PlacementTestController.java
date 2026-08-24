182008df8a8d35fc41327fc551a89010b4c08bee
ChinhCDHE181478 <quansuper000@gmail.com>
2026-08-24 15:26:18 +0700

    delete v2 controller

--- backend/src/main/java/fu/sep490/g23/backend/controllerv2/placementtest/PlacementTestController.java
+++
@@ -1,9 +0,0 @@
-package fu.sep490.g23.backend.controllerv2.placementtest;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/v2/student/placement-tests")
-public class PlacementTestController {
-}
--- backend/src/main/java/fu/sep490/g23/backend/servicev2/placementtest/IPlacementTestService.java
+++
@@ -1,8 +0,0 @@
-package fu.sep490.g23.backend.servicev2.placementtest;
-
-import fu.sep490.g23.backend.dto.request.assessment.PlacementTestSubmissionRequest;
-import fu.sep490.g23.backend.dto.response.assessment.PlacementTestAttemptResponse;
-
-public interface IPlacementTestService {
-    PlacementTestAttemptResponse submit(PlacementTestSubmissionRequest request, String studentEmail);
-}
