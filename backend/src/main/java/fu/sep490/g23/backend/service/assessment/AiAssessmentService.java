5db7418ef8313779ee4ff7152d1a254734dae674
ChinhCDHE181478 <quansuper000@gmail.com>
2026-08-30 15:22:31 +0700

    u

--- backend/src/main/java/fu/sep490/g23/backend/service/assessment/AiAssessmentService.java
+++ backend/src/main/java/fu/sep490/g23/backend/service/assessment/AiAssessmentService.java
@@ -6,6 +6,6 @@ import fu.sep490.g23.backend.dto.response.assessment.CourseAssessmentResponse;
 import java.util.List;
 
 public interface AiAssessmentService {
- 
+    List<CourseAssessmentResponse> getCourseAssessments(Long courseId, String studentEmail);
     AiAssessmentSubmissionResponse submitAssessment(Long assessmentId, AssessmentSubmissionRequest request, String studentEmail);
 }
